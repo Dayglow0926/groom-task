b0ddb704626130052840afb1f45ec502145d3e53
Dayglow0926 <dayglow0926@gmail.com>
2023-12-06 20:06:53 +0900

    move: 파일 위치 변경

--- react-netflix-clone/src/api/axios.js
+++ react-netflix-clone/src/api/axios.js
@@ -3,8 +3,8 @@ import axios from "axios";
 const instance = axios.create({
   baseURL: "https://api.themoviedb.org/3",
   params: {
-    api_key: "6cd0e176c3eeff11651f09836c220109",
-    language: "ko-KR",
+    api_key: "a0e72862806797c82c4d4fa4fdbdd684",
+    language: "ko-kr",
   },
 });
 
