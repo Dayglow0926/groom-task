4cd7817049ecaf2740ee859de958bdeb060b1c33
Dayglow0926 <dayglow0926@gmail.com>
2023-12-07 23:45:34 +0900

    feat: 검색 기능 추가

--- react-netflix-clone/src/components/Nav.js
+++ react-netflix-clone/src/components/Nav.js
@@ -2,9 +2,12 @@ import React, { useEffect, useState } from "react";
 import netflixLogo from "../assets/icon/netflix_logo.png";
 import userLogged from "../assets/icon/login_avatar.png";
 import "./Nav.css";
+import { Link, useNavigate } from "react-router-dom";
 
 function Nav() {
   const [show, setShow] = useState(false);
+  const [searchValue, setSearchValue] = useState("");
+  const navigate = useNavigate();
 
   useEffect(() => {
     window.addEventListener("scroll", (e) => {
@@ -20,14 +23,25 @@ function Nav() {
     };
   }, []);
 
+  const handleChange = (e) => {
+    setSearchValue(e.target.value);
+    navigate(`/search?q=${e.target.value}`);
+  };
+
   return (
     <nav className={`nav ${show ? "nav__black" : ""}`}>
-      <img
-        src={netflixLogo}
-        alt="Netflix logo"
-        className="nav__logo"
-        onClick={() => window.location.reload()}
+      <Link to={"/"} className="nav__logo-link">
+        <img src={netflixLogo} alt="Netflix logo" className="nav__logo" />
+      </Link>
+
+      <input
+        value={searchValue}
+        onChange={handleChange}
+        className="nav__input"
+        type="text"
+        placeholder="영화를 검색해주세요."
       />
+
       <img src={userLogged} alt="User logged" className="nav__avatar" />
     </nav>
   );
